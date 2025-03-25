fb9137fd7f7b2e32a4e3056c6b8dbd3f066d1429
Ana Cezanoski <juliacezanoski@gmail.com>
2025-03-25 18:40:42 -0300

    Estrutura de User

---
+++ OngGatoBackend/gatosong/src/main/java/br/com/pucpr/gatosong/donation/service/impl/DefaultUserService.java
@@ -0,0 +1,88 @@
+package br.com.pucpr.gatosong.donation.service.impl;
+
+import br.com.pucpr.gatosong.donation.model.UserModel;
+import br.com.pucpr.gatosong.donation.repository.UserRepository;
+import br.com.pucpr.gatosong.donation.service.UserService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+@NoArgsConstructor
+public class DefaultUserService implements UserService {
+
+    private static final Logger logger = LogManager.getLogger(DefaultUserService.class);
+
+    private UserRepository userRepository;
+
+    @Override
+    public List<UserModel> getAllUsers() {
+
+        List<UserModel> list = userRepository.findAll();
+
+        if (list.isEmpty()) {
+            return null;
+        }
+
+        return list;
+    }
+
+    @Override
+    public List<UserModel> getUserById(Long id) {
+
+        Optional<UserModel> userModel = userRepository.findById(id);
+
+        return userModel.map(Collections::singletonList).orElse(null);
+    }
+
+    @Override
+    public List<UserModel> createUser(UserModel userModel) {
+
+        UserModel model = null;
+
+        if(!ObjectUtils.isEmpty(userModel)) {
+            try {
+                model = userRepository.save(userModel);
+            }catch (Exception e) {
+                logger.error("Unable to save UserModel", e);
+            }
+        }
+
+        return ObjectUtils.isEmpty(model)? null : Collections.singletonList(model);
+    }
+
+    @Override
+    public UserModel updateUser(UserModel userModel) {
+
+        UserModel model = null;
+
+        if(!ObjectUtils.isEmpty(userModel) && userRepository.existsById(userModel.getId())) {
+            try {
+                model = userRepository.save(userModel);
+            }catch (Exception e) {
+                logger.error("Unable to save UserModel", e);
+            }
+        }
+
+        return ObjectUtils.isEmpty(model)? null : model;
+    }
+
+    @Override
+    public void deleteUserModel(Long id) {
+        if(userRepository.existsById(id)) {
+            try {
+                userRepository.deleteById(id);
+            }catch (Exception e) {
+                logger.error("Unable to delete UserModel", e);
+            }
+        }
+    }
+}
