2a0ca4690431a7b69cb5d83d7e2c383793f6f0c0
Kau <kauanesdrk@gmail.com>
2025-04-18 12:53:16 -0300

    :zap::navbar placeholder

--- OngGatoFrontend/ong-gato-app-front/src/Components/Layout/OngPage.tsx
+++ OngGatoFrontend/ong-gato-app-front/src/Components/Layout/OngPage.tsx
@@ -3,9 +3,14 @@ import { Outlet } from "react-router-dom"
 
 
 export const OngPage = () => {
+    const navbarPlaceholder = "aqui-é-a-barra-de-navegação"
+
     return (
         <div className="flex w-full bg-slate-50 h-screen">
-            <div role="navigation" className="h-screen bg-white w-[100px] border-1 border-slate-200">
+            <div role="navigation" className="flex flex-col justify-center items-center h-screen bg-white w-[100px] border-1 border-slate-200">
+                {navbarPlaceholder.split('').map((t)=>(
+                    <div>{t}</div>
+                ))}
             </div>
 
             <div className="w-full rounded rounded-md">
