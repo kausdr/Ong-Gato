f4cc542ea83f25174ed68f874590f20c9f90eb94
Ana Cezanoski <juliacezanoski@gmail.com>
2025-06-10 17:25:35 -0300

    correções conflitos

--- OngGatoFrontend/ong-gato-app-front/src/Components/Layout/OngPage.tsx
+++ OngGatoFrontend/ong-gato-app-front/src/Components/Layout/OngPage.tsx
@@ -4,21 +4,14 @@ import Button from "./Button";
 import { useNavigate } from "react-router-dom";
 import { RiListView } from "react-icons/ri";
 import { BiDonateHeart } from "react-icons/bi";
-import { IoHomeOutline } from "react-icons/io5";
 import { MdOutlineManageAccounts } from "react-icons/md";
 import { BsBarChartLine } from "react-icons/bs";
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import { LiaCatSolid } from 'react-icons/lia'
-import { FaInstagram, FaFacebookF, FaTwitter, FaWhatsapp } from "react-icons/fa"
-import { useState } from "react";
 import { IoPersonCircleOutline } from "react-icons/io5";
 import { useAuth } from "../../Contexts/AuthContext";
 
 export const OngPage = () => {
     const navigate = useNavigate()
     const { pathname } = useLocation();
-    const [isOpen, setIsOpen] = useState(false)
     const { user } = useAuth();
 
     const activePage = {
@@ -28,43 +21,25 @@ export const OngPage = () => {
         gerenciar: pathname.includes("/gerenciar"),
         relatorio: pathname.includes("/relatorio"),
         perfil: pathname.includes("/perfil"),
-        homePage: pathname.includes("/homePage"),
-        signout: pathname.includes("#")
     };
 
-    const toggleDropdown = () => {
-        setIsOpen(!isOpen)
-    }
-
-    const handleMouseLeave = () => {
-        setIsOpen(false)
-    }
 
     return (
-        <div className="min-h-full">
-            <Disclosure as="nav" className="bg-blue-200">
-                <div className="w-full px-4 sm:px-6 lg:px-8">
-                    <div className="flex h-16 items-center ">
-                        <div className="w-full flex items-center justify-between">
-                            <div className="flex flex-row gap-1 items-center">
-                                <img src="/Images/OngLogo.png" alt="Logo" className="w-10 h-10 rounded-full" />
-                            </div>
-                            <div className="hidden md:block w-full">
-                                <div className="flex w-full justify-between">
-                                    <div className="flex">
-                                    <Button
-                                        order={"nav"}
-                                        className={` text-white ${activePage.homePage ? "!text-white" : "text-white"} `}
-                                        text={"Home Page"}
-                                        action={() => { navigate("/homePage") }}
-                                        className={`justify-start  ${activePage.homePage ? " text-white bg-blue-400 " : "text-white "}`} />
+        <div className="flex w-full bg-slate-50 h-screen">
+            <div role="navigation" className="flex flex-col justify-between gap-2 py-5 h-screen bg-white w-[200px] border-1 border-slate-200">
+                <div className="flex flex-col gap-10">
+                    <div className="flex flex-col gap-2 items-center">
+                        <img src="/OngLogo.png" alt="Logo" className="w-16 h-16 rounded-full" />
+                        <p className="text-sky-600">Meownager</p>
+                    </div>
 
-                                    <Button
-                                        order={"nav"}
-                                        className={` text-white ${activePage.doar ? "!text-white" : "text-white"} `}
-                                        text={"Fazer Doação"}
-                                        action={() => { navigate("/doar") }}
-                                        className={`justify-start ${activePage.doar ? " text-white bg-blue-400" : "text-white"}`}/>
+                    <div className="flex flex-col w-full">
+                        <Button
+                            order={"nav"}
+                            icon={<LuHistory className={`text-xl text-slate-500 ${activePage.historico ? "!text-sky-700" : ""} `} />}
+                            text={"Histórico"}
+                            action={() => { navigate("/historico") }}
+                            className={`justify-start ${activePage.historico ? "bg-sky-100 text-sky-700" : ""}`} />
 
                         {user && user.isAdmin && (
                             <>
@@ -85,12 +60,13 @@ export const OngPage = () => {
                             </>
                         )}
 
-                                    <Button
-                                        order={"nav"}
-                                        className={` text-white ${activePage.gerenciar ? "!text-white" : "text-white"} `}
-                                        text={"Gerenciar"}
-                                        action={() => { navigate("/gerenciar") }}
-                                        className={`justify-start ${activePage.gerenciar ? " text-white bg-blue-400" : "text-white"}`}/>
+                        <Button
+                            order={"nav"}
+                            icon={<BiDonateHeart className={`text-xl text-slate-500 ${activePage.doar ? "!text-sky-700" : ""} `} />}
+                            text={"Doar"}
+                            action={() => { navigate("/doar") }}
+                            className={`justify-start ${activePage.doar ? "bg-sky-100 text-sky-700" : ""}`}
+                        />
 
                         <Button
                             order={"nav"}
@@ -106,9 +82,9 @@ export const OngPage = () => {
                     order={"nav"}
                     icon={
                         user && user.profilePicture ? (
-                            <img 
-                                src={user.profilePicture} 
-                                alt="Foto de perfil" 
+                            <img
+                                src={user.profilePicture}
+                                alt="Foto de perfil"
                                 className="w-8 h-8 rounded-full object-cover"
                             />
                         ) : (
@@ -124,6 +100,7 @@ export const OngPage = () => {
             <div className="w-full rounded rounded-md">
                 <Outlet />
             </div>
+
         </div>
     )
 }
--- OngGatoFrontend/ong-gato-app-front/src/Screens/Donate/Donate.tsx
+++ OngGatoFrontend/ong-gato-app-front/src/Screens/Donate/Donate.tsx
@@ -1,7 +1,6 @@
 import Input from "../../Components/data-input/Input";
 import { CiMail } from "react-icons/ci";
 import { FaBrazilianRealSign, FaInstagram, FaFacebookF, FaTwitter, FaWhatsapp } from "react-icons/fa6";
-import { FaBrazilianRealSign } from "react-icons/fa6";
 import Button from "../../Components/Layout/Button";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
@@ -182,77 +181,6 @@ function Donate() {
         </div>
 
     )
-}
-
-function Donate() {
-  const navigate = useNavigate();
-  const [donateValue, setDonateValue] = useState<number>(0);
-  const [canDonate, setCanDonate] = useState<boolean>(false);
-  const [pixCode, setPixCode] = useState<boolean>(false);
-  useEffect(() => {
-    setCanDonate(donateValue > 0);
-  }, [donateValue]);
-
-  return (
-    <>
-      <div className="min-h-full">
-        <div className="w-full h-screen flex justify-center bg-white items-center">
-          <Card className="flex flex-col gap-10 p-5 bg-white max-w-[500px]">
-            {!pixCode ? (
-              <div className="flex flex-col gap-10">
-                <div className="flex flex-col gap-5">
-                  <h1 className="text-xl font-bold">Doar</h1>
-                  <p className="text-slate-600">Após escolher um valor, iremos gerar um código PIX para você realizar a doação por meio do seu banco de preferência.</p>
-                </div>
-                <div className="grid grid-cols-3 gap-3">
-                  {[10, 40, 55, 90, 150, 240].map((val) => (
-                    <Button
-                      key={val}
-                      order="secondary"
-                      text={val.toString()}
-                      icon={<FaBrazilianRealSign className="text-green-600" />}
-                      action={() => setDonateValue(val)}
-                    />
-                  ))}
-                </div>
-
-                <Input
-                  label="Deseja inserir um valor personalizado?"
-                  type="number"
-                  icon={<FaBrazilianRealSign className="text-green-600" />}
-                  id="valor"
-                  name="valor"
-                  placeholder="Digite um valor para doar, ex: 15"
-                  value={donateValue}
-                  setValue={setDonateValue}
-                  mandatory={false}
-                />
-
-                <Button
-                  order={canDonate ? "primary" : "inactive"}
-                  text="Doar"
-                  action={() => setPixCode(true)}
-                />
-              </div>
-            ) : (
-              <div className="flex flex-col gap-5">
-                <h3 className="text-xl font-bold">Nós e nossos pets agradecemos a doação!</h3>
-                <p>Seu código PIX está sendo gerado...</p>
-                <Button
-                  order="primary"
-                  text="Doar mais"
-                  action={() => {
-                    setPixCode(false);
-                    setDonateValue(0);
-                  }}
-                />
-              </div>
-            )}
-          </Card>
-        </div>
-      </div>
-    </>
-  );
 }
 
 export default Donate;
