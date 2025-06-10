9e8cc169406cb7ecfd575bd119509f758dc624db
Ana Cezanoski <juliacezanoski@gmail.com>
2025-06-10 18:28:56 -0300

    correções telas

--- OngGatoFrontend/ong-gato-app-front/src/Components/Layout/OngPage.tsx
+++ OngGatoFrontend/ong-gato-app-front/src/Components/Layout/OngPage.tsx
@@ -1,106 +1,115 @@
-import { Outlet, useLocation } from "react-router-dom"
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { useState } from "react";
 import { LuHistory } from "react-icons/lu";
-import Button from "./Button";
-import { useNavigate } from "react-router-dom";
 import { RiListView } from "react-icons/ri";
 import { BiDonateHeart } from "react-icons/bi";
+import { IoHomeOutline, IoPersonCircleOutline } from "react-icons/io5";
 import { MdOutlineManageAccounts } from "react-icons/md";
 import { BsBarChartLine } from "react-icons/bs";
-import { IoPersonCircleOutline } from "react-icons/io5";
+import { Disclosure } from '@headlessui/react';
+import Button from "./Button";
 import { useAuth } from "../../Contexts/AuthContext";
 
 export const OngPage = () => {
-    const navigate = useNavigate()
-    const { pathname } = useLocation();
-    const { user } = useAuth();
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const { user } = useAuth();
+  const [isOpen, setIsOpen] = useState(false);
 
-    const activePage = {
-        historico: pathname.includes("/historico"),
-        doadores: pathname.includes("/doadores"),
-        doar: pathname.includes("/doar"),
-        gerenciar: pathname.includes("/gerenciar"),
-        relatorio: pathname.includes("/relatorio"),
-        perfil: pathname.includes("/perfil"),
-    };
+  const activePage = {
+    historico: pathname.includes("/historico"),
+    doadores: pathname.includes("/doadores"),
+    doar: pathname.includes("/doar"),
+    gerenciar: pathname.includes("/gerenciar"),
+    relatorio: pathname.includes("/relatorio"),
+    perfil: pathname.includes("/perfil"),
+    homePage: pathname.includes("/homePage"),
+  };
 
+  const toggleDropdown = () => setIsOpen(!isOpen);
+  const handleMouseLeave = () => setIsOpen(false);
 
-    return (
-        <div className="flex w-full bg-slate-50 h-screen">
-            <div role="navigation" className="flex flex-col justify-between gap-2 py-5 h-screen bg-white w-[200px] border-1 border-slate-200">
-                <div className="flex flex-col gap-10">
-                    <div className="flex flex-col gap-2 items-center">
-                        <img src="/OngLogo.png" alt="Logo" className="w-16 h-16 rounded-full" />
-                        <p className="text-sky-600">Meownager</p>
-                    </div>
+  return (
+    <div className="min-h-full">
+      <Disclosure as="nav" className="bg-blue-200">
+        <div className="w-full px-4 sm:px-6 lg:px-8">
+          <div className="flex h-16 items-center justify-between">
+            <div className="flex flex-row gap-1 items-center">
+              <img src="/Images/OngLogo.png" alt="Logo" className="w-10 h-10 rounded-full" />
+            </div>
+            <div className="hidden md:block w-full">
+              <div className="flex justify-between">
+                <div className="flex gap-1">
+                  <Button
+                    order="nav"
+                    text="Home Page"
+                    action={() => navigate("/homePage")}
+                    className={`justify-start ${activePage.homePage ? "bg-blue-100 text-blue-400" : "text-white"}`}
+                  />
+                  <Button
+                    order="nav"
+                    text="Fazer Doação"
+                    action={() => navigate("/doar")}
+                    className={`justify-start ${activePage.doar ? "bg-blue-100 text-blue-400" : "text-white"}`}
+                  />
+                  <Button
+                    order="nav"
+                    text="Relatórios"
+                    action={() => navigate("/relatorio")}
+                    className={`justify-start ${activePage.relatorio ? "bg-blue-100 text-blue-400" : "text-white"}`}
+                  />
+                  <Button
+                    order="nav"
+                    text="Histórico de Doações"
+                    action={() => navigate("/historico")}
+                    className={`justify-start ${activePage.historico ? "bg-blue-100 text-blue-400" : "text-white"}`}
+                  />
 
-                    <div className="flex flex-col w-full">
-                        <Button
-                            order={"nav"}
-                            icon={<LuHistory className={`text-xl text-slate-500 ${activePage.historico ? "!text-sky-700" : ""} `} />}
-                            text={"Histórico"}
-                            action={() => { navigate("/historico") }}
-                            className={`justify-start ${activePage.historico ? "bg-sky-100 text-sky-700" : ""}`} />
-
-                        {user && user.isAdmin && (
-                            <>
-                                <Button
-                                    order={"nav"}
-                                    icon={<RiListView className={`text-xl text-slate-500 ${activePage.doadores ? "!text-sky-700" : ""} `} />}
-                                    text={"Doadores"}
-                                    action={() => { navigate("/doadores") }}
-                                    className={`justify-start ${activePage.doadores ? "bg-sky-100 text-sky-700" : ""}`}
-                                />
-                                <Button
-                                    order={"nav"}
-                                    icon={<MdOutlineManageAccounts className={`text-xl text-slate-500 ${activePage.gerenciar ? "!text-sky-700" : ""} `} />}
-                                    text={"Gerenciar"}
-                                    action={() => { navigate("/gerenciar") }}
-                                    className={`justify-start ${activePage.gerenciar ? "bg-sky-100 text-sky-700" : ""}`}
-                                />
-                            </>
-                        )}
-
-                        <Button
-                            order={"nav"}
-                            icon={<BiDonateHeart className={`text-xl text-slate-500 ${activePage.doar ? "!text-sky-700" : ""} `} />}
-                            text={"Doar"}
-                            action={() => { navigate("/doar") }}
-                            className={`justify-start ${activePage.doar ? "bg-sky-100 text-sky-700" : ""}`}
-                        />
-
-                        <Button
-                            order={"nav"}
-                            icon={<BsBarChartLine className={`text-xl text-slate-500 ${activePage.relatorio ? "!text-sky-700" : ""} `} />}
-                            text={"Relatório"}
-                            action={() => { navigate("/relatorio") }}
-                            className={`justify-start ${activePage.relatorio ? "bg-sky-100 text-sky-700" : ""}`}
-                        />
-                    </div>
+                  {user && user.isAdmin && (
+                    <>
+                      <Button
+                        order="nav"
+                        icon={<RiListView className={`text-xl ${activePage.doadores ? "text-sky-700" : "text-slate-500"}`} />}
+                        text="Doadores"
+                        action={() => navigate("/doadores")}
+                        className={`justify-start ${activePage.doadores ? "bg-sky-100 text-sky-700" : "text-white"}`}
+                      />
+                      <Button
+                        order="nav"
+                        icon={<MdOutlineManageAccounts className={`text-xl ${activePage.gerenciar ? "text-blue-400" : "text-blue-400"}`} />}
+                        text="Gerenciar"
+                        action={() => navigate("/gerenciar")}
+                        className={`justify-start ${activePage.gerenciar ? "bg-blue-100 text-blue-400" : "text-white"}`}
+                      />
+                    </>
+                  )}
                 </div>
-
-                <Button
-                    order={"nav"}
-                    icon={
+                    <Button
+                      order="nav"
+                      icon={
                         user && user.profilePicture ? (
-                            <img
-                                src={user.profilePicture}
-                                alt="Foto de perfil"
-                                className="w-8 h-8 rounded-full object-cover"
-                            />
+                          <img
+                            src={user.profilePicture}
+                            alt="Foto de perfil"
+                            className="w-8 h-8 rounded-full object-cover"
+                          />
                         ) : (
-                            <IoPersonCircleOutline className={`text-3xl text-slate-500 ${activePage.perfil ? "!text-sky-700" : ""}`} />
+                          <IoPersonCircleOutline className={`text-3xl ${activePage.perfil ? "text-blue-400" : "text-blue-400"}`} />
                         )
-                    }
-                    text={"Perfil"}
-                    action={() => { navigate("/perfil") }}
-                    className={`justify-center items-center ${activePage.perfil ? "bg-sky-100 text-sky-700" : ""}`}
-                />
+                      }
+                      text="Perfil"
+                      action={() => navigate("/perfil")}
+                      className={`justify-center items-center ${activePage.perfil ? "bg-blue-100 text-blue-400" : "text-blue-400"}`}
+                    />
+              </div>
             </div>
-
-            <div className="w-full rounded rounded-md">
-                <Outlet />
-            </div>
-
+          </div>
         </div>
-    )
-}
+      </Disclosure>
+
+      <div className="w-full rounded-md">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
